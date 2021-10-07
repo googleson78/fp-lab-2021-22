7ffdac9218b4b64df487b61485935b0442d2480b
Georgi Lyubenov <godzbanebane@gmail.com>
2021-10-07 16:58:58 +0300

    Add some tasks if we get to them

--- exercises/00/Intro.hs
+++ exercises/00/Intro.hs
@@ -1,3 +1,8 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}     -- cover all cases!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}          -- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}      -- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}          -- use different names!
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-} -- warn about incomplete patterns v2
 module Intro where
 
 -- TODO: > whoami
@@ -50,3 +55,59 @@ module Intro where
 -- pattern matching
 -- if
 -- operators
+
+{-
+-- use pattern matches!
+-- >>> fact 5
+-- 120
+-- >>> fact 7
+-- 5040
+fact :: Integer -> Integer
+fact n = _
+
+-- >>> fib 0
+-- 0
+-- >>> fib 4
+-- 3
+-- >>> fib 8
+-- 21
+fib :: Integer -> Integer
+fib = _
+
+-- use the following "mathematical definition" to implement addition on natural numbers:
+-- myPlus x y = y                if x == 0
+-- myPlus x y = succ(pred(x) + y) else
+-- note that succ and pred are alrady available
+myPlus :: Integer -> Integer -> Integer
+myPlus n m = _
+
+-- same as above, implement multiplication on natural numbers recursively, using pred and myPlus
+myMult :: Integer -> Integer -> Integer
+myMult n m = _
+
+-- use the property that x^(2*n) == (x*x)^n
+-- this is logarithmic, instead of linear, in the second argument
+-- >>> fastPow 3 4
+-- 81
+-- >>> fastPow 2 6
+-- 64
+fastPow :: Integer -> Integer -> Integer
+fastPow = _
+
+-- rem x y = what's the remainder of x when divided by y
+-- >>> isPrime 5
+-- True
+-- >>> isPrime 6
+-- False
+-- >>> isPrime 13
+-- True
+isPrime :: Integer -> Bool
+isPrime n = _
+  where
+    divides :: Integer -> Integer -> Bool
+    divides x y = _
+
+    -- iterate over all the values in the given range, and check if any of them divide n
+    anyDividesInRange :: Integer -> Integer -> Bool
+    anyDividesInRange start end = _
+-}
