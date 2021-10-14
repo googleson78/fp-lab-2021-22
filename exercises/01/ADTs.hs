ff3baaadfeb53c333ed8b52edfcbb9d3c11b3ddd
Georgi Lyubenov <godzbanebane@gmail.com>
2021-10-14 16:30:13 +0300

    Finish up 01

--- exercises/01/ADTs.hs
+++ exercises/01/ADTs.hs
@@ -15,42 +15,86 @@ module ADTs where
 -- * pragmas on the top of files
 -- * first homework?
 
--- struct Point {
---   int x;
---   int y;
--- };
--- Point somePoint = Point{5, 3};
-
 -- show:
--- "enum"
--- * RPS
+-- * remind about sections
+-- * mention inline evaluation of expressions
+-- * RPS - "enum"
 -- * ask about gadt or normal
 -- * mention Bool
 -- * deriving Show
--- product types - tuple?
--- sum types - animal, animaltype
-
-data Colour
-data Breed
-
-data Animal
--- recursive types - Nat.
--- "calculator"
 
 data RPS
 
 beats :: RPS -> RPS -> Bool
 beats = undefined
 
--- task
+-- struct Point {
+--   float x;
+--   float y;
+-- };
+-- Point somePoint = Point{5, 3};
+
+-- regular syntax
+data Point
+
+somePoint :: Point
+somePoint = undefined
+
+-- gadt syntax
+data Point1 where
+
+somePoint1 :: Point1
+somePoint1 = undefined
+
+isInFirstQuadrant :: Point -> Bool
+isInFirstQuadrant = undefined
+
+invert :: Point -> Point
+invert = undefined
+
+data Colour
+
+data Breed
+
+data Animal
+
+data Nat
+
+addNat :: Nat -> Nat -> Nat
+addNat = undefined
+
+natToInteger :: Nat -> Integer
+natToInteger = undefined
+
+-- >>> integerToNat 0
+-- Zero
+-- >>> integerToNat 3
+-- Suc (Suc (Suc Zero))
+integerToNat :: Integer -> Nat
+integerToNat = undefined
+
+-- TASK
+-- define what the "next" throw you can do is in the "usual" ordering of RPS
+-- i.e. @next x@ should be the throw that beats x
 next :: RPS -> RPS
 next = undefined
 
--- task
+-- TASK
+-- define what it means for two RPS values to be equal
+-- in general for a type, this would mean that the constructors must be equal
+-- and all their contents should all so be equal (pointwise)
+-- for an "enum" in particular, this only leaves the constructor check
+-- EXAMPLES
+-- >>> eqRPS Rock Rock
+-- True
+-- >>> eqRPS Rock Paper
+-- False
 eqRPS :: RPS -> RPS -> Bool
 eqRPS = undefined
 
--- task
+-- TASK
+-- define a shorter version of beats by uisng next and eqRPS
+-- EXAMPLES
 -- >>> beats' Rock Paper
 -- True
 -- >>> beats' Rock Scissors
@@ -60,56 +104,112 @@ eqRPS = undefined
 beats' :: RPS -> RPS -> Bool
 beats' = undefined
 
-data Point = MkPoint Int Int
-
-somePoint :: Point
-somePoint = MkPoint 5 3
-
-data Point1 where
-  MkPoint1 :: Int -> Int -> Point1
-
-somePoint1 :: Point1
-somePoint1 = MkPoint1 5 3
-
-isInFirstQuadrant :: Point -> Bool
-isInFirstQuadrant = undefined
-
-invert :: Point -> Point
-invert = undefined
-
-data Nat
-
-addNat :: Nat -> Nat -> Nat
-addNat = undefined
-
--- task
+-- TASK
+-- multiply two @Nat@s recursively, much like we did with Ints last time
+-- EXAMPLES
+-- >>> multNat Zero (Suc (Suc (Suc Zero)))
+-- Zero
+-- >>> multNat (integerToNat 2) (integerToNat 3)
+-- Suc (Suc (Suc (Suc (Suc (Suc Zero)))))
 multNat :: Nat -> Nat -> Nat
-multNat = undefined
+multNat n m = undefined
 
--- task
+-- TASK
+-- calculate the larger of two @Nat@s recursively
+-- EXAMPLES
+-- >>> maxNat (Suc Zero) Zero
+-- Suc Zero
+-- >>> maxNat (Suc (Suc Zero)) Zero
+-- Suc (Suc Zero)
+-- >>> maxNat (Suc (Suc Zero)) (Suc (Suc (Suc (Suc Zero))))
+-- Suc (Suc (Suc (Suc Zero)))
 maxNat :: Nat -> Nat -> Nat
-maxNat = undefined
+maxNat n m = undefined
 
--- task
--- circle or square
+-- TASK
+-- Ordering is a datatype that is made to mean "the result of a comparison" or "the ordering between two things"
+-- it's defined like so:
+-- @data Ordering = LT | EQ | GT@
+-- with the constructors being L(ess)T(han), EQ(ual) G(reater)T(han)
+-- implement a comparison for @Nat@s, returning an @Ordering@
+-- EXAMPLES
+-- >>> compareNat (Suc Zero) (Suc Zero)
+-- EQ
+-- >>> compareNat Zero (Suc Zero)
+-- LT
+-- >>> compareNat (Suc Zero) Zero
+-- GT
+compareNat :: Nat -> Nat -> Ordering
+compareNat n m = undefined
+
+-- TASK
+-- create a Shape datatype that is either a Circle or a Rectangle,
+-- containing the required @Point@s and/or @Float@s to define them in an ordinary 2d coordinate system
 data Shape
 
-area :: Shape -> Int
-area = undefined
+-- TASK
+-- Calculate the area of a Shape
+-- note the existence of the constant @pi@ and the function @abs@ to calculate absolute values
+area :: Shape -> Float
+area s = undefined
 
-data Calc
-  = Val Int
-  | Plus Int Int
-  | Mult Int Int
+-- the "syntax" for a very basic "calculator" datatype
+-- or alternatively, a very simple programming language
+--
+-- we can build up Expr(essions) by
+-- * injecting integers as a value directly - Val
+-- * stating that we want to add the result of two calculations - Plus
+-- * stating that we want to multiply the result of two calculations - Mult
+data Expr
+  = Val Integer
+  | Plus Expr Expr
+  | Mult Expr Expr
+  deriving Show
 
--- task
-eval :: Calc -> Int
-eval = undefined
+-- we can abuse sections to write "prettier" expressions
 
--- task add if, consider 0 false and /=0 true, extend eval
+-- using these pragmas we set the associativity of the constructor functions
+-- so that
+-- Val 3 `Plus` Val 4 `Plus` Val 5
+-- means
+-- Val 3 `Plus` (Val 4 `Plus` Val 5)
+-- we also define Mult with higher precedence, so that we can write the usual
+-- Val 3 `Plus` Val 4 `Mult` Val 5
+-- to mean
+-- Val 3 `Plus` (Val 4 `Mult` Val 5)
+-- infixr(ight)
+infixr 7 `Plus`
+infixr 8 `Mult`
 
--- task, maybe
+-- TASK
+-- and now that we have the syntactic structure of the computation we want to make
+-- we can implement its semantics by writing an evaluator for our calculator
+-- or alternatively an interpreter for our programming language
+-- EXAMPLES
+-- >>> eval (Val 3)
+-- 3
+-- >>> eval (Plus (Val 3) (Val 4))
+-- 7
+-- >>> eval (Val 33 `Mult` Val 36)
+-- 1188
+-- >>> eval ((Val 3 `Plus` Val 3) `Mult` Val 7)
+-- 42
+-- >>> eval (Val 3 `Plus` Val 3 `Mult` Val 7)
+-- 24
+eval :: Expr -> Integer
+eval expr = undefined
+
+-- TASK
+-- add an If expression to our Expr language
+-- by using other calculations for our "condition value"
+-- extend eval so that it also works with the new If construction
+-- interpreting 0 as "false" and any other value as "true"
+
+-- TASK
 -- add "the noise that the animal makes" to the Animal type
--- is there a way to refactor it so that
+-- how should you model a "noise"?
+-- how are you going to add it to the Animal type?
+--
+-- is the way you've added it more optimal - or is there something you could factor out so that
 -- speak :: Animal -> String
--- is easier to write, than matching on Dog and Cat?
+-- is easier to write, without matching on both Dog and Cat?
